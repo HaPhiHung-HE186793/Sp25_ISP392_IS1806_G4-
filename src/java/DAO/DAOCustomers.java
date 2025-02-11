431e670d3facbf9cb62e47f6cc5192d24f402a20
quangminh-0391 <minhpqhe182304@fpt.edu.vn>
2025-02-11 17:31:02 +0700

    fix DAOCustomers

--- src/java/DAO/DAOCustomers.java
+++ src/java/DAO/DAOCustomers.java
@@ -37,10 +37,10 @@ public class DAOCustomers extends DBContext {
             double totalDebt = rs.getDouble("totalDebt");  // Sử dụng double cho tổng nợ
             String createAt = rs.getString("createAt");
             String updateAt = rs.getString("updateAt");
-            String createBy = rs.getString("createBy");
-            int isDelete = rs.getInt("isDelete");  // Chuyển sang boolean
+            int createBy = rs.getInt("createBy");
+            Boolean isDelete = rs.getBoolean("isDelete");  // Chuyển sang boolean
             String deleteAt = rs.getString("deleteAt");
-            String deleteBy = rs.getString("deleteBy");
+            int deleteBy = rs.getInt("deleteBy");
 
             customers customer = new customers(customerID, name, email, phone, address, totalDebt, createAt, updateAt, createBy, isDelete, deleteAt, deleteBy);
             vector.add(customer);
@@ -74,10 +74,10 @@ public class DAOCustomers extends DBContext {
         pre.setDouble(5, customer.getTotalDebt()); // totalDebt
         pre.setString(6, customer.getCreateAt()); // createAt
         pre.setString(7, customer.getUpdateAt()); // updateAt
-        pre.setString(8, customer.getCreateBy()); // createBy
-        pre.setInt(9, customer.getIsDelete()); // isDelete
+        pre.setInt(8, customer.getCreateBy()); // createBy
+        pre.setBoolean(9, customer.isIsDelete()); // isDelete
         pre.setString(10, customer.getDeleteAt()); // deleteAt
-        pre.setString(11, customer.getDeleteBy()); // deleteBy
+        pre.setInt(11, customer.getDeleteBy()); // deleteBy
         pre.setInt(12, customer.getCustomerID()); // customerID
         n = pre.executeUpdate();
     } catch (SQLException ex) {
@@ -97,43 +97,16 @@ public class DAOCustomers extends DBContext {
         pre.setDouble(5, customer.getTotalDebt()); // totalDebt
         pre.setString(6, customer.getCreateAt()); // createAt
         pre.setString(7, customer.getUpdateAt()); // updateAt
-        pre.setString(8, customer.getCreateBy()); // createBy
-        pre.setInt(9, customer.getIsDelete()); // isDelete
+        pre.setInt(8, customer.getCreateBy()); // createBy
+        pre.setBoolean(9, customer.isIsDelete()); // isDelete
         pre.setString(10, customer.getDeleteAt()); // deleteAt
-        pre.setString(11, customer.getDeleteBy()); // deleteBy
+        pre.setInt(11, customer.getDeleteBy()); // deleteBy
         n = pre.executeUpdate();
     } catch (SQLException ex) {
         Logger.getLogger(DAOCustomers.class.getName()).log(Level.SEVERE, null, ex);
     }
     return n;
-}
-//     public void listAll() {
-//    String sql = "SELECT * FROM customers"; // Cập nhật tên bảng
-//    try {
-//        Statement state = conn.createStatement();
-//        ResultSet rs = state.executeQuery(sql);
-//        while (rs.next()) {
-//            int customerID = rs.getInt("customerID");
-//            String name = rs.getString("name");
-//            String email = rs.getString("email");
-//            String phone = rs.getString("phone");
-//            String address = rs.getString("address");
-//            double totalDebt = rs.getDouble("totalDebt"); // Sử dụng double cho tổng nợ
-//            String createAt = rs.getString("createAt");
-//            String updateAt = rs.getString("updateAt");
-//            String createBy = rs.getString("createBy");
-//            int isDelete = rs.getInt("isDelete"); // Giả sử isDelete là kiểu int
-//            String deleteAt = rs.getString("deleteAt");
-//            String deleteBy = rs.getString("deleteBy");
-//
-//            customers customer = new customers(customerID, name, email, phone, address, totalDebt, createAt, updateAt, createBy, isDelete, deleteAt, deleteBy);
-//            System.out.println(customer);
-//        }
-//    } catch (SQLException ex) {
-//        ex.printStackTrace();
-//    } 
-//}
-//     
+}  
      
      public List<customers> listAll() {
     List<customers> list = new ArrayList<>();
@@ -155,7 +128,7 @@ public class DAOCustomers extends DBContext {
             String deleteAt = rs.getString("deleteAt");
             String deleteBy = rs.getString("deleteBy");
 
-            customers customer = new customers(customerID, name, email, phone, address, totalDebt, createAt, updateAt, createBy, isDelete, deleteAt, deleteBy);
+            customers customer = new customers(customerID, name, email, phone, address, totalDebt, createAt, updateAt, isDelete, true, deleteAt, isDelete);
             list.add(customer); // Thêm vào danh sách thay vì in ra
         }
     } catch (SQLException ex) {
@@ -169,7 +142,7 @@ public class DAOCustomers extends DBContext {
     DAOCustomers dao = new DAOCustomers();
 
     // 1. Thêm một khách hàng mới
-    customers newCustomer = new customers( "Nguyễn Văn C", "nguyenvanC@gmail.com", "0123456789", "123 Đường ABC", 1000.0, "2023-01-01", "2023-01-01", "1", 0, null, null);
+    customers newCustomer = new customers( "Nguyễn Văn D", "nguyenvanC@gmail.com", "0123456789", "123 Đường ABC", 1000.0, "2023-01-01", "2023-01-01", 1, false, null, 0);
     int insertResult = dao.insertCustomer(newCustomer);
     System.out.println("Insert result: " + insertResult);
 
