12b23f9764e3a8b1b14c2ffd1ed385820c8059c2
quangminh-0391 <minhpqhe182304@fpt.edu.vn>
2025-02-12 14:55:54 +0700

    fix ZOnes

---
+++ src/java/Entity/Zones.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Zones {
+    private int zoneID;
+    private String zoneName;
+    private String createAt; // Có thể sử dụng LocalDateTime nếu cần
+    private String updateAt; // Có thể sử dụng LocalDateTime nếu cần
+    private int createBy;
+    private boolean isDelete; // Giả sử isDelete là kiểu int
+    private String deleteAt; // Có thể sử dụng LocalDateTime nếu cần
+    private int deleteBy;
+
+    public Zones() {
+    }
+
+    public Zones(int zoneID, String zoneName, String createAt, String updateAt, int createBy, boolean isDelete, String deleteAt, int deleteBy) {
+        this.zoneID = zoneID;
+        this.zoneName = zoneName;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.createBy = createBy;
+        this.isDelete = isDelete;
+        this.deleteAt = deleteAt;
+        this.deleteBy = deleteBy;
+    }
+
+    public Zones(String zoneName, String createAt, String updateAt, int createBy, boolean isDelete, String deleteAt, int deleteBy) {
+        this.zoneName = zoneName;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.createBy = createBy;
+        this.isDelete = isDelete;
+        this.deleteAt = deleteAt;
+        this.deleteBy = deleteBy;
+    }
+
+    public int getZoneID() {
+        return zoneID;
+    }
+
+    public void setZoneID(int zoneID) {
+        this.zoneID = zoneID;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(String updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public int getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(int createBy) {
+        this.createBy = createBy;
+    }
+
+    public boolean isIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public String getDeleteAt() {
+        return deleteAt;
+    }
+
+    public void setDeleteAt(String deleteAt) {
+        this.deleteAt = deleteAt;
+    }
+
+    public int getDeleteBy() {
+        return deleteBy;
+    }
+
+    public void setDeleteBy(int deleteBy) {
+        this.deleteBy = deleteBy;
+    }
+
+    
+    
+}
