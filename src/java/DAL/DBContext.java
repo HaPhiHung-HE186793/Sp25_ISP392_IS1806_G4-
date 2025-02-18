d6076c8df282ad3cc6cd5af8b884e70e76ac7f88
NguyenNhatAnh <anh13.9.04@gmail.com>
2025-02-19 01:34:21 +0700

    update listusers

--- src/java/DAL/DBContext.java
+++ src/java/DAL/DBContext.java
@@ -39,7 +39,7 @@ public class DBContext {
         }
     }
     public DBContext() {
-        this("jdbc:sqlserver://localhost:1433;databaseName=ISPG4NV1","sa","123456");
+        this("jdbc:sqlserver://10.211.55.3;databaseName=ISPG4NV1","sa","123");
     }
     public void dispatch(HttpServletRequest request, HttpServletResponse response, String page) {
     RequestDispatcher dispatcher = request.getRequestDispatcher(page);
