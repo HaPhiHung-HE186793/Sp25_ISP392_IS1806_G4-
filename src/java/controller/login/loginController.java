07fcf1c11b660a6462cea359029980e6f0b75bd4
quangminh-0391 <minhpqhe182304@fpt.edu.vn>
2025-02-19 14:50:16 +0700

    sua method login

--- src/java/controller/login/loginController.java
+++ src/java/controller/login/loginController.java
@@ -35,7 +35,37 @@ public class loginController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        HttpSession session=request.getSession(true);
+        
+            
+        }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+      //  processRequest(request, response);
+      HttpSession session=request.getSession(true);
         session.setMaxInactiveInterval(30 * 60);
         DAOUser dao = new DAOUser();
         String service = request.getParameter("service");
@@ -86,35 +116,6 @@ public class loginController extends HttpServlet {
     }
 }
 }
-            
-        }
-    
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
-
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /** 
@@ -125,19 +126,5 @@ public class loginController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-private String hashPassword(String password){
-        try{
-            MessageDigest md = MessageDigest.getInstance("MD5");     
-            md.update(password.getBytes());      
-            byte[] digest = md.digest();     
-            StringBuilder sb = new StringBuilder();     
-            for (byte b : digest) {         
-                sb.append(String.format("%02x", b));
-                
-            }
-            return sb.toString();
-        }catch (NoSuchAlgorithmException ex){
-            return null;
-        }
-    }
+
 }
