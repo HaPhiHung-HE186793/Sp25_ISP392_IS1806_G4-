58f3f26d7b2a5198116957a4bed8d175404c2f2a
quangminh-0391 <minhpqhe182304@fpt.edu.vn>
2025-03-07 16:31:44 +0700

    them model.CustomerOrder

---
+++ src/java/model/CustomerOrder.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CustomerOrder {
+    public int orderID;
+    public String name;
+    public String email;
+    public String phone;
+
+    public CustomerOrder() {
+    }
+
+    public CustomerOrder(int orderID, String name, String email, String phone) {
+        this.orderID = orderID;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
